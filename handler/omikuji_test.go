853ac04a959b03d1706d50f0570af05360902870
shuntaka9576 <shuntaka9576@gmail.com>
2018-09-14 20:41:35 +0900

    Add coverage.

--- handler/omikuji_test.go
+++ handler/omikuji_test.go
@@ -1,7 +1,25 @@
 package handler_test
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/shuntaka9576/omikuji/handler"
+)
 
 func TestOmikujiHandler(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handler.OmikujiHandler))
+	defer ts.Close()
 
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error("unexpected")
+		return
+	}
+
+	if res.StatusCode != 200 {
+		t.Error("Status code error")
+		return
+	}
 }
